3f4bf8b3eb0fe7d309bea030f4e503aac3ba90ff
puentesabel15 <puentesabel15@gmail.com>
2022-12-31 12:50:08 -0500

    Refactor Program

--- SocialMediaApi/Program.cs
+++ SocialMediaApi/Program.cs
@@ -1,89 +1,31 @@
-using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using SocialMedia.Core.CustomEntities;
-using SocialMedia.Core.Data;
-using SocialMedia.Core.Interfaces;
-using SocialMedia.Core.Services;
+using SocialMedia.Infrastructure.Extensions;
 using SocialMedia.Infrastructure.Filters;
-using SocialMedia.Infrastructure.Interfaces;
-using SocialMedia.Infrastructure.Options;
-using SocialMedia.Infrastructure.Repositories;
-using SocialMedia.Infrastructure.Services;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers().AddNewtonsoftJson(options =>
-{
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-})
-.ConfigureApiBehaviorOptions(options =>
-{
-    //options.SuppressModelStateInvalidFilter = true;
-});
+builder.AddDbContexts();
 
-builder.Services.Configure<PaginationOptions>(builder.Configuration.GetSection("Pagination"));
+builder.AddOptions();
 
-builder.Services.Configure<PasswordsOptions>(builder.Configuration.GetSection("PasswordOptions"));
+builder.AddServices();
 
-builder.Services.AddSingleton<IPasswordService, PasswordService>() ;
+builder.AddJwtAuthentication();
+
+builder.AddMVCServices();
 
 builder.Services.AddControllers(options => options.Filters.Add<GlobalExceptionFilters>());
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddTransient<IPostService, PostService>();
+builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddTransient<ISecurityService, SecurityService>();
-
-builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
-//builder.Services.AddSingleton<IUriService, UriService>();
-builder.Services.AddSingleton<IUriService>(provider =>
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
 {
-    var accesor = provider.GetRequiredService<IHttpContextAccessor>();
-    var request = accesor.HttpContext.Request;
-    var absoluteUri= string.Concat(request.Scheme, "://",request.Host.ToUriComponent());
-    return new UriService(absoluteUri);
-});
-builder.Services.AddScoped(typeof(SocialMedia.Core.Interfaces.IRepository<>),typeof(BaseRepository<>));
-
-builder.Services.AddDbContext<SocialMediaContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("SocialMedia")));
-//builder.Services.Configure<PaginationOptions>(Configuration.GetSection("Pagination"));
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Authentication:Isser"],
-        ValidAudience = builder.Configuration["Authentication:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Authentication:SecretKey"]))
-    };
-});
-
-builder.Services.AddMvc(options =>
-{
-    options.Filters.Add<ValidationFilter>();
-}).AddFluentValidation(options =>
-{
-    options.RegisterValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
-});
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+}).ConfigureApiBehaviorOptions(options =>{});
 
 var app = builder.Build();
 
@@ -91,16 +33,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        //options.SwaggerEndpoint("/swagger/v1/swagger.json", "Social Media Api V1.0.1");
-        //options.RoutePrefix =string.Empty;
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
